8501b7cdb4eec53ac0564064c19133a904d936a1
sonkybyu <nakpakpan@byupathway.edu>
2026-05-23 01:15:48 +0100

    week03

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,6 @@ document.getElementById('currentyear').textContent = currentYear;
 
 document.getElementById('lastModified').textContent =
   `Last Modified: ${document.lastModified}`;
+// const para = document.querySelector('.para');
+
+// para.innerHTML += "<h2>New Title</h2><p>Updated content!</p>"
